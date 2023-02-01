9574c948b2c238789140d70cdd72d3f78b17afa5
Baptiste Morvan <bapmorvan@gmail.com>
2023-02-01 20:07:34 +0100

    set souspages + responsive

--- src/Pages/SousPage/SousPage.js
+++ src/Pages/SousPage/SousPage.js
@@ -4,27 +4,84 @@ import { useParams } from 'react-router-dom';
 import '../../styles/pages/sousPage.scss'
 import Footer from '../../components/Footer/footer'
 import Navbar from '../../components/Header/Navbar';
-import data from '../../components/Data.json';
+import data from '../../components/Data';
+
 
 function SousPage() {
   const { id } = useParams();
   
-  const [pageData, setPageData] = useState({});
+  const [item, setItem] = useState({});
 
   useEffect(() => {
-    const page = data.pages.find((page) => page.id === id);
-    setPageData(page);
-  }, [id]);
 
+    const itemData = data.find(item => item.id === id);
+    setItem(itemData);
+  }, [id]);
 
   return (
     <div className="page-sousPage">
       <Navbar />
         <div className="sousPage-content">
-          <h2 className="sousPage-title" >{pageData.title}</h2>
+          <h2 className="sousPage-title" >{item.title}</h2>
         </div>
+        <div className="sousPage-titleImg-content">
+          <img className="titleImg-content" src={item.imgTitle} alt="computer" />
+        </div>
+        <div className="sousPage-title2-content">
+          <h3 className="title2-content">nos services</h3>
+        </div>
+        <div className="sousPage-section-service">
+          <article className="sousPage-article">
+            <div className="sousPage-article-left">
+              <div className="sousPage-info-content">
+                <div className="info-content-img">
+                  <img src={item.imgTitle1} alt="img1" />
+                </div>
+                <div className="info-content-title">
+                  <h3>{item.title1}</h3>
+                  <p>{item.parag1}</p>
+                </div>
+              </div>
+              <div className="sousPage-info-content">
+                <div className="info-content-img">
+                  <img src={item.imgTitle2} alt="img1" />
+                </div>
+                <div className="info-content-title">
+                  <h3>{item.title2}</h3>
+                  <p>{item.parag2}</p>
+                </div>
+              </div>
+              <div className="sousPage-info-content">
+                <div className="info-content-img">
+                  <img src={item.imgTitle3} alt="img1" />
+                </div>
+                <div className="info-content-title">
+                  <h3>{item.title3}</h3>
+                  <p>{item.parag3}</p>
+                </div>
+              </div>
+              <div className="sousPage-info-content">
+                <div className="info-content-img">
+                  <img src={item.imgTitle4} alt="img1" />
+                </div>
+                <div className="info-content-title">
+                  <h3>{item.title4}</h3>
+                  <p>{item.parag4}</p>
+                </div>
+              </div> 
+            </div>
+          </article>
+          <div className="sousPage-right-img">
+            <img className="right-img" src={item.imgDroite} alt="" />
+          </div>
+        </div>
+        <div className="sousPage-title2-content">
+          <h3 className="title2-content">nos réalisations</h3>
+        </div>
+      <div className="footer-padding">
+        <Footer />  
+      </div>
       
-      <Footer />
     </div>
   );
 }
---
+++ src/components/Data.js
@@ -0,0 +1,80 @@
+import imgBranding from '../styles/assets/img/imgSousPages/imgBranding.png'
+import imgBranding1 from '../styles/assets/img/imgSousPages/imgBranding1.svg'
+import imgBranding2 from '../styles/assets/img/imgSousPages/imgBranding2.svg'
+import imgBranding3 from '../styles/assets/img/imgSousPages/imgBranding3.svg'
+import imgBranding4 from '../styles/assets/img/imgSousPages/imgBranding4.svg'
+import imgDroiteBranding from '../styles/assets/img/imgSousPages/imgDroiteBranding.png'
+
+import imgWeb from '../styles/assets/img/imgSousPages/imgWeb.png'
+import imgWeb1 from '../styles/assets/img/imgSousPages/imgWeb1.svg'
+import imgWeb2 from '../styles/assets/img/imgSousPages/imgWeb2.svg'
+import imgWeb3 from '../styles/assets/img/imgSousPages/imgWeb3.svg'
+import imgWeb4 from '../styles/assets/img/imgSousPages/imgWeb4.svg'
+import imgDroiteWeb from '../styles/assets/img/imgSousPages/imgDroiteWeb.png'
+
+
+import imgMedia from '../styles/assets/img/imgSousPages/imgMedia.png'
+import imgMedia1 from '../styles/assets/img/imgSousPages/imgMedia1.svg'
+import imgMedia2 from '../styles/assets/img/imgSousPages/imgMedia2.svg'
+import imgMedia3 from '../styles/assets/img/imgSousPages/imgMedia3.svg'
+import imgMedia4 from '../styles/assets/img/imgSousPages/imgMedia4.svg'
+import imgDroiteMedia from '../styles/assets/img/imgSousPages/imgDroiteMedia.png'
+
+
+const data = [
+      {
+        id: "Branding",
+        title: "identité visuelle",
+        imgTitle: imgBranding,
+        title1: "image de marque",
+        imgTitle1: imgBranding1,
+        parag1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title2: "création de logo",
+        imgTitle2: imgBranding2,
+        parag2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title3: "print",
+        imgTitle3: imgBranding3,
+        parag3:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title4: "packaging",
+        imgTitle4: imgBranding4,
+        parag4:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        imgDroite: imgDroiteBranding
+      },
+      {
+        id: "SiteInternet",
+        title: "site internet",
+        imgTitle: imgWeb,
+        title1: "site vitrine",
+        imgTitle1: imgWeb1,
+        parag1:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title2: "seo / sea",
+        imgTitle2: imgWeb2,
+        parag2:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title3: "site e-commerce",
+        imgTitle3: imgWeb3,
+        parag3:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title4: "développement web",
+        imgTitle4: imgWeb4,
+        parag4:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        imgDroite: imgDroiteWeb
+      },
+      {
+        id: "Reseauxsociaux",
+        title: "médias sociaux",
+        imgTitle: imgMedia,
+        title1: "stratégie social media",
+        imgTitle1: imgMedia1,
+        parag1:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title2: "community management",
+        imgTitle2: imgMedia2,
+        parag2:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title3: "contenus",
+        imgTitle3: imgMedia3,
+        parag3:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        title4: "publicité",
+        imgTitle4: imgMedia4,
+        parag4:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sed facere nobis pariatur voluptatem, molestias asperiores voluptate quae ullam praesentium! Rerum autem natus assumenda labore odio repellat quae deserunt.",
+        imgDroite: imgDroiteMedia
+      }
+    ]
+    export default data;
